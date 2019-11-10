f18c6a64b478b008779afb06e05602e4e307b2d6
Komei Yamaguchi <ymgc1994@icloud.com>
2019-11-11 00:17:00 +0900

    completed

---
+++ src/editor/editor.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import ReactQuill from 'react-quill'
+import debounce from '../helpers'
+import BorderColorIcon from '@material-ui/icons/BorderColor'
+import { withStyles } from '@material-ui/core/styles'
+import styles from './styles'
+
+export class EditorComponent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: '',
+      title: '',
+      id: ''
+    }
+  }
+  componentDidMount = () => {
+    this.setState({
+      text: this.props.selectedNote.body,
+      title: this.props.selectedNote.title,
+      id: this.props.selectedNote.id
+    })
+  }
+  componentDidUpdate = () => {
+    if (this.props.selectedNote.id !== this.state.id) {
+      this.setState({
+        text: this.props.selectedNote.body,
+        title: this.props.selectedNote.title,
+        id: this.props.selectedNote.id
+      })
+    }
+  }
+  render() {
+    const { classes } = this.props
+    return (
+      <div className={classes.editorContainer}>
+        <BorderColorIcon className={classes.editIcon}></BorderColorIcon>
+        <input
+          className={classes.titleInput}
+          placeholder="Note title..."
+          value={this.state.title ? this.state.title : ''}
+          onChange={e => this.updateTitle(e.target.value)}
+        ></input>
+        <ReactQuill
+          value={this.state.text}
+          onChange={this.updateBody}
+        ></ReactQuill>
+      </div>
+    )
+  }
+  updateBody = async val => {
+    await this.setState({ text: val })
+    this.updata()
+  }
+  updateTitle = async val => {
+    await this.setState({ title: val })
+    this.updata()
+  }
+  updata = debounce(() => {
+    this.props.noteUpdate(this.state.id, {
+      title: this.state.title,
+      body: this.state.text
+    })
+    // come back later
+  }, 1500)
+}
+// this.props.classes.className
+export default withStyles(styles)(EditorComponent)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require('firebase')
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyCyXQMXx3QIC8TqAm9IGvu2oQ-aNIpb-jg',
+  authDomain: 'evernote-clone-5a6af.firebaseapp.com',
+  databaseURL: 'https://evernote-clone-5a6af.firebaseio.com',
+  projectId: 'evernote-clone-5a6af',
+  storageBucket: 'evernote-clone-5a6af.appspot.com',
+  messagingSenderId: '988877858348',
+  appId: '1:988877858348:web:c9bbaf41bb69ebda9d9389',
+  measurementId: 'G-NRNM2KKPQ3'
+})
+// firebase.analytics()
+ReactDOM.render(<App />, document.getElementById('evernote-container'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
